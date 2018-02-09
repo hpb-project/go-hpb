00f6d33607062192138ae1ace4f4be748d7640a5
duanhong <837670785@qq.com>
2018-02-09 18:58:51 +0800

    ghpb

--- go-hpb/cmd/ghpb/main.go
+++ go-hpb/cmd/ghpb/main.go
@@ -140,7 +140,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = ghpb
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2017 The go-hpb Authors "
+	app.Copyright = "Copyright 2013-2018 The go-hpb Authors "
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
