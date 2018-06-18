c61b0d92176737856d35cbe60d4eb571f3c2aadc
luxq <xueqian1991@gmail.com>
2018-06-18 15:43:09 +0800

    add wait queue.

---
+++ boe/core/type.h
@@ -0,0 +1,17 @@
+// Last Update:2018-06-18 15:36:31
+/**
+ * @file type.h
+ * @brief 
+ * @author luxueqian
+ * @version 0.1.00
+ * @date 2018-06-16
+ */
+
+#ifndef TYPE_H
+#define TYPE_H
+
+
+typedef uint8_t TVersion;
+typedef void*   Handle_t;
+
+#endif  /*TYPE_H*/
