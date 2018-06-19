0111febb81d3fa8a645702b81a7b4c54d1a5202b
codex8080 <codex8080@163.com>
2018-06-19 10:38:19 +0800

    iperf server ok

---
+++ network/p2p/iperf/iperf_test.go
@@ -0,0 +1,30 @@
+// Copyright 2018 The go-hpb Authors
+// This file is part of the go-hpb.
+//
+// The go-hpb is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-hpb is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-hpb. If not, see <http://www.gnu.org/licenses/>.
+
+package iperf
+
+import (
+	"testing"
+)
+
+var (
+	iperf = &Iperf{SrvPort:5201,CliHost:"127.0.0.1",CliPort:5201}
+)
+
+func TestValidateSign(t *testing.T) {
+
+	iperf.StartSever()
+}
