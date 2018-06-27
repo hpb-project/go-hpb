a12ccf8a9a5da4e335303177b5e2051cc6daf572
loushl <loushl123@sina.com>
2018-06-27 20:52:33 +0800

    change symode function

--- config/nodeconfig.go
+++ config/nodeconfig.go
@@ -28,6 +28,51 @@ const (
 	LightSync                 // Download only the headers and terminate afterwards
 )
 
+func (mode SyncMode) IsValid() bool {
+	return mode >= FullSync && mode <= LightSync
+}
+
+// String implements the stringer interface.
+func (mode SyncMode) String() string {
+	switch mode {
+	case FullSync:
+		return "full"
+	case FastSync:
+		return "fast"
+	case LightSync:
+		return "light"
+	default:
+		return "unknown"
+	}
+}
+
+func (mode SyncMode) MarshalText() ([]byte, error) {
+	switch mode {
+	case FullSync:
+		return []byte("full"), nil
+	case FastSync:
+		return []byte("fast"), nil
+	case LightSync:
+		return []byte("light"), nil
+	default:
+		return nil, fmt.Errorf("unknown sync mode %d", mode)
+	}
+}
+
+func (mode *SyncMode) UnmarshalText(text []byte) error {
+	switch string(text) {
+	case "full":
+		*mode = FullSync
+	case "fast":
+		*mode = FastSync
+	case "light":
+		*mode = LightSync
+	default:
+		return fmt.Errorf(`unknown sync mode %q, want "full", "fast" or "light"`, text)
+	}
+	return nil
+}
+
 var DefaultConfig = Nodeconfig{
 	SyncMode:    FastSync,
 	DataDir:     DefaultDataDir(),
