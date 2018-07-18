9385b5dc79f8cd98a188c3765446188c82a7fd8f
rjgeek <rxl635@student.bham.ac.uk>
2018-07-18 17:12:50 +0100

    add function of setHpbbase

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -440,8 +440,8 @@ web3._extend({
 			call: 'miner_stop'
 		}),
 		new web3._extend.Method({
-			name: 'setEtherbase',
-			call: 'miner_setEtherbase',
+			name: 'setHpberbase',
+			call: 'miner_setHpberbase',
 			params: 1,
 			inputFormatter: [web3._extend.formatters.inputAddressFormatter]
 		}),
