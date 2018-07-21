be922182122429162e1307372ce558c0cd1531f3
codex8080 <codex8080@163.com>
2018-07-21 10:32:31 +0800

    version 0.3.0 for transaction test.

--- config/version.go
+++ config/version.go
@@ -28,8 +28,8 @@ const (
 )
 const (
 	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 1        // Minor version component of the current release
-	VersionPatch = 1        // Patch version component of the current release
+	VersionMinor = 3        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = "dev" // Version metadata to append to the version string
 )
 
