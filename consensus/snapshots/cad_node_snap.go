714102c40d2f6277fcade468883a9351ff2d16f2
mym-xjl <xujiali5172923>
2018-08-23 22:14:05 +0800

    chang log.

--- consensus/snapshots/cad_node_snap.go
+++ consensus/snapshots/cad_node_snap.go
@@ -126,7 +126,7 @@ func CalcuCadNodeSnap(db hpbdb.Database, number uint64, hash common.Hash, header
 func LoadCadNodeSnap(db hpbdb.Database, hash common.Hash) (*CadNodeSnap, error) {
 	blob, err := db.Get(append([]byte("codnodesnap-"), hash[:]...))
 	if err != nil {
-		log.Error("Log Read Failed1:", err)
+		log.Error("Log Read Failed1:", "err:", err)
 		return nil, err
 	}
 	cadNodeSnap := new(CadNodeSnap)
