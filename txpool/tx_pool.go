1a12fd4c9662d09d7c4abf33cef15d3ea146aa5a
hansxiaole <316821049@qq.com>
2018-09-21 17:12:21 +0800

    Error log modi Trace log

--- txpool/tx_pool.go
+++ txpool/tx_pool.go
@@ -380,7 +380,7 @@ func (pool *TxPool) AddTxs(txs []*types.Transaction) error {
 		}
 		// If the transaction fails basic validation, discard it
 		if err := pool.validateTx(tx); err != nil {
-			log.Error("Discarding invalid transaction", "hash", hash, "err", err)
+			log.Trace("Discarding invalid transaction", "hash", hash, "err", err)
 			return err
 		}
 	}
