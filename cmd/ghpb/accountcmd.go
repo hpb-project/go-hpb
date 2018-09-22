a5906a6c5b324edff96fe16c992b56d6e31c82be
codex8080 <codex8080@163.com>
2018-09-22 12:26:09 +0800

    change log info

--- cmd/ghpb/accountcmd.go
+++ cmd/ghpb/accountcmd.go
@@ -219,7 +219,6 @@ func unlockAccount(ctx *cli.Context, ks *keystore.KeyStore, address string, i in
 		prompt := fmt.Sprintf("Unlocking account %s | Attempt %d/%d", address, trials+1, 3)
 
 		password := getPassPhrase(prompt, false, i, passwords)
-		log.Error("---------the password file is ",": ", password)
 		err = ks.Unlock(account, password)
 		if err == nil {
 			log.Info("Unlocked account", "address", account.Address.Hex())
