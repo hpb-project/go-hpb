31c91040468867da648ae4e928efae00ad948404
codex8080 <codex8080@163.com>
2018-09-25 07:06:53 +0800

    version of 1.0.2.2

--- config/version.go
+++ config/version.go
@@ -30,7 +30,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionHardv = 0        // Hardware version component of the current release
 	VersionMinor = 2        // Minor version component of the current release
-	VersionPatch = 1        // Patch version component of the current release
+	VersionPatch = 2        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
