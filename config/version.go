20bf945de6b1d108d4b7e28d8fb9d64b2114d971
xueqianLu <xueqian1991@163.com>
2018-11-13 20:26:20 +0800

    change version to v1.0.3.0

--- config/version.go
+++ config/version.go
@@ -29,8 +29,8 @@ const (
 const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionHardv = 0        // Hardware version component of the current release
-	VersionMinor = 2        // Minor version component of the current release
-	VersionPatch = 3        // Patch version component of the current release
+	VersionMinor = 3        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
