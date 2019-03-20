62b77085e97dcf440ddabd89d892a4607d21236b
liuziqiang <adwen2010@163.com>
2019-03-20 16:22:17 +0800

    set ttl=1m

--- synctrl/syncer.go
+++ synctrl/syncer.go
@@ -394,15 +394,17 @@ func (this *Syncer) qosTuner() {
 // requestTTL returns the current timeout allowance for a single sync request
 // to finish under.
 func (this *Syncer) requestTTL() time.Duration {
-	var (
-		rtt  = time.Duration(atomic.LoadUint64(&this.rttEstimate))
-		conf = float64(atomic.LoadUint64(&this.rttConfidence)) / 1000000.0
-	)
-	ttl := time.Duration(ttlScaling) * time.Duration(float64(rtt)/conf)
-	if ttl > ttlLimit {
-		ttl = ttlLimit
-	}
-	return ttl
+	/*
+		var (
+			rtt  = time.Duration(atomic.LoadUint64(&this.rttEstimate))
+			conf = float64(atomic.LoadUint64(&this.rttConfidence)) / 1000000.0
+		)
+		ttl := time.Duration(ttlScaling) * time.Duration(float64(rtt)/conf)
+		if ttl > ttlLimit {
+			ttl = ttlLimit
+		}
+	*/
+	return ttlLimit
 }
 
 // stateFetcher manages the active state sync and accepts requests
