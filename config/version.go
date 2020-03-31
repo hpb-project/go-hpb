c36dfb87556f5719b08664926959f136d1b4b86e
luxq <xueqian1991@gmail.com>
2020-03-31 12:28:57 +0800

    change versin to 1.0.6.4

--- config/version.go
+++ config/version.go
@@ -34,7 +34,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionHardv = 0        // Hardware version component of the current release
 	VersionMinor = 6        // Minor version component of the current release
-	VersionPatch = 3        // Patch version component of the current release
+	VersionPatch = 4        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
