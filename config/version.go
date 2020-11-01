84dd724978cc9952110ac9e6e3150a392bf98311
rjgeek <rujia.li@hpb.io>
2020-11-01 18:11:37 +0800

    add branch prepare 1.0.7.2

--- config/version.go
+++ config/version.go
@@ -30,11 +30,11 @@ const (
 */
 const VersionID uint64 = 0x0002
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionHardv = 0        // Hardware version component of the current release
-	VersionMinor = 7        // Minor version component of the current release
-	VersionPatch = 1        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1          // Major version component of the current release
+	VersionHardv = 0          // Hardware version component of the current release
+	VersionMinor = 7          // Minor version component of the current release
+	VersionPatch = 2          // Patch version component of the current release
+	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
