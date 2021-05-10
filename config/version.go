2165d020bc4ff7cde15eb06889e17f5c7a5774f9
rjgeek <rujia.li@hpb.io>
2021-05-10 14:01:10 +0800

    change version to 1.0.7.6

--- config/version.go
+++ config/version.go
@@ -34,7 +34,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionHardv = 0        // Hardware version component of the current release
 	VersionMinor = 7        // Minor version component of the current release
-	VersionPatch = 5        // Patch version component of the current release
+	VersionPatch = 6        // Patch version component of the current release
 	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
