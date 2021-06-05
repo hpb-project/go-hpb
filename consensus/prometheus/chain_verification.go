1deec45c2be7b833400ade89e3fa2b803a000fe2
adwen2010 <adwen2010@163.com>
2021-06-05 19:21:03 +0800

    election

--- consensus/prometheus/chain_verification.go
+++ consensus/prometheus/chain_verification.go
@@ -369,7 +369,9 @@ func (c *Prometheus) GetSelectPrehp(state *state.StateDB, chain consensus.ChainR
 	var err error
 	var bootnodeinfp []p2p.HwPair
 	log.Debug("GetSelectPrehp", "number", header.Number.Uint64(), "consensus.NewContractVersion", consensus.NewContractVersion)
-	if header.Number.Uint64() > consensus.NewContractVersion {
+	if header.Number.Uint64() > consensus.StageNumberElection {
+		err, bootnodeinfp = c.GetNodeinfoFromElectContract(chain, header, state)
+	} else if header.Number.Uint64() > consensus.NewContractVersion {
 		err, bootnodeinfp = c.GetNodeinfoFromNewContract(chain, header, state)
 	} else {
 		err, bootnodeinfp = c.GetNodeinfoFromContract(chain, header, state)
@@ -398,6 +400,7 @@ func (c *Prometheus) GetSelectPrehp(state *state.StateDB, chain consensus.ChainR
 	}
 	addrlist := make([]common.Address, 0, len(bootnodeinfp))
 	for _, v := range bootnodeinfp {
+		log.Warn("hpbnodes", "v=", common.HexToAddress(strings.Replace(v.Adr, " ", "", -1)))
 		addrlist = append(addrlist, common.HexToAddress(strings.Replace(v.Adr, " ", "", -1)))
 	}
 
@@ -422,7 +425,9 @@ func (c *Prometheus) GetSelectPrehp(state *state.StateDB, chain consensus.ChainR
 
 	//get all votes
 	var voteres map[common.Address]big.Int
-	if header.Number.Uint64() > consensus.NewContractVersion {
+	if header.Number.Uint64() > consensus.StageNumberElection {
+		err, voteres = c.GetVoteResFromElectionContract(chain, header, state)
+	} else if header.Number.Uint64() > consensus.NewContractVersion {
 		err, voteres = c.GetVoteResFromNewContract(chain, header, state)
 	} else {
 		err, _, voteres = c.GetVoteRes(chain, header, state)
@@ -440,7 +445,14 @@ func (c *Prometheus) GetSelectPrehp(state *state.StateDB, chain consensus.ChainR
 	bandrank, errbandwith := c.GetBandwithRes(addrlist, chain, number-1)
 	//get all balances
 	var allbalances map[common.Address]big.Int
-	if header.Number.Uint64() > consensus.NewContractVersion {
+	if header.Number.Uint64() > consensus.StageNumberElection {
+		errs, hpblist, coinaddresslist := c.GetCoinAddressFromElectionContract(chain, header, state)
+		if errs != nil || coinaddresslist == nil || len(coinaddresslist) == 0 || hpblist == nil || len(hpblist) == 0 {
+			log.Error("CoinAddress ERR", "errs", errs)
+		}
+		log.Warn("GetCoinAddressFromElectionContract", "lenhpblist", len(hpblist), "coinaddresslist", len(coinaddresslist))
+		allbalances, err = c.GetAllBalancesByCoin(hpblist, coinaddresslist, state)
+	} else if header.Number.Uint64() > consensus.NewContractVersion {
 		errs, hpblist, coinaddresslist := c.GetCoinAddressFromNewContract(chain, header, state)
 		if errs != nil || coinaddresslist == nil || len(coinaddresslist) == 0 || hpblist == nil || len(hpblist) == 0 {
 			log.Error("CoinAddress ERR", "errs", errs)
