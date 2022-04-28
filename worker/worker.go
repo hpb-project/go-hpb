1a7567bc6189001d6fcda7a2c71ee7a6db16161f
rjgeek <rjgeek@hpb.io>
2022-04-28 20:43:57 +0800

    modify max tx number.

--- worker/worker.go
+++ worker/worker.go
@@ -50,7 +50,7 @@ const (
 	// chainSideChanSize is the size of channel listening to ChainSideEvent.
 	chainSideChanSize = 10
 
-	blockMaxTxs = 5000 * 10
+	blockMaxTxs = 8000 * 10
 )
 
 // Agent can register themself with the worker
