578aa39d876f5250a7271eadf6a477344b05a91d
rjgeek <rjgeek@hpb.io>
2022-05-02 13:51:05 +0800

    add new network version.

--- vmcore/vm/evm.go
+++ vmcore/vm/evm.go
@@ -15,7 +15,8 @@ import (
 
 func NewEVM(config *config.ChainConfig, msg vmcore.Message, header *types.Header, chain vmcore.ChainContext,
 	author *common.Address, statedb vmcore.StateDB) vmcore.EVM {
-	if header.Number.Uint64() < consensus.StageNumberEvmV2 {
+	blocknumber := header.Number.Uint64()
+	if blocknumber < consensus.StageNumberEvmV2 {
 		cfg := hevm.Config{}
 		// Create a new context to be used in the EVM environment
 		context := hvm.NewEVMContext(msg, header, chain, author)
@@ -23,17 +24,26 @@ func NewEVM(config *config.ChainConfig, msg vmcore.Message, header *types.Header
 		// about the transaction and calling mechanisms.
 		vmenv := hevm.NewEVM(context, statedb, config, cfg)
 		return vmenv
-	} else {
+	} else if blocknumber >= consensus.StageNumberEvmV2 && blocknumber < consensus.StateNumberEvmRevert {
 		cfg := eevm.Config{}
 		txContext := evm.NewEVMTxContext(msg)
 		blockContext := evm.NewEVMBlockContext(header, chain, author)
 		return eevm.NewEVM(blockContext, txContext, statedb, config, cfg)
+	} else { //if blocknumber >= consensus.StateNumberEvmRevert
+		cfg := hevm.Config{}
+		// Create a new context to be used in the EVM environment
+		context := hvm.NewEVMContext(msg, header, chain, author)
+		// Create a new environment which holds all relevant information
+		// about the transaction and calling mechanisms.
+		vmenv := hevm.NewEVM(context, statedb, config, cfg)
+		return vmenv
 	}
 }
 
 func NewEVMForGeneration(config *config.ChainConfig, header *types.Header,
 	author common.Address, statedb vmcore.StateDB, getHash func(uint64) common.Hash, gasPrice int64) vmcore.EVM {
-	if header.Number.Uint64() < consensus.StageNumberEvmV2 {
+	blocknumber := header.Number.Uint64()
+	if blocknumber < consensus.StageNumberEvmV2 {
 		context := hevm.Context{
 			CanTransfer: vmcore.CanTransfer,
 			Transfer:    vmcore.Transfer,
@@ -49,7 +59,7 @@ func NewEVMForGeneration(config *config.ChainConfig, header *types.Header,
 		cfg := hevm.Config{}
 		vmenv := hevm.NewEVM(context, statedb, config, cfg)
 		return vmenv
-	} else {
+	} else if blocknumber >= consensus.StageNumberEvmV2 && blocknumber < consensus.StateNumberEvmRevert {
 		// todo: add evm
 		cfg := eevm.Config{}
 		txContext := eevm.TxContext{
@@ -69,5 +79,21 @@ func NewEVMForGeneration(config *config.ChainConfig, header *types.Header,
 			Random:      extra.GetSignedLastRND()[:32],
 		}
 		return eevm.NewEVM(blockContext, txContext, statedb, config, cfg)
+	} else { // if blocknumber >= consensus.StateNumberEvmRevert
+		context := hevm.Context{
+			CanTransfer: vmcore.CanTransfer,
+			Transfer:    vmcore.Transfer,
+			GetHash:     getHash,
+			Origin:      author,
+			Miner:       author,
+			BlockNumber: new(big.Int).Set(header.Number),
+			Time:        new(big.Int).Set(header.Time),
+			Difficulty:  new(big.Int).Set(header.Difficulty),
+			GasLimit:    new(big.Int).Set(header.GasLimit),
+			GasPrice:    new(big.Int).Set(big.NewInt(gasPrice)),
+		}
+		cfg := hevm.Config{}
+		vmenv := hevm.NewEVM(context, statedb, config, cfg)
+		return vmenv
 	}
 }
